9f35a37985e677553c3124bb6a26f145c5828cb2
kseesala <seesala.kiran@gmail.com>
2017-12-26 23:40:40 -0600

    Updated code

--- src/test/java/com/siva/sandbox/SimpleTest.java
+++ src/test/java/com/siva/sandbox/SimpleTest.java
@@ -1,25 +1,35 @@
 package com.siva.sandbox;
 
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.when;
+
 import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
-
 public class SimpleTest {
+	/**
+	 * Constants
+	 */
 	public static String HOST = "http://rest-test-cases.getsandbox.com";
-	public static String PATH_HEARTBEAT = "/api/heartbeat";
-	public static String PATH_SAMPLE = "/api/sample";
+	public static String API = "/api";
+	public static String PATH_HEARTBEAT = "/heartbeat";
+	public static String PATH_SAMPLE = "/sample";
 
 	@BeforeClass
 	public void checkIfWebServiceIsUp() {
+		baseURI = HOST;
+		basePath = API;
+		
 		/**
 		 * Check if the Webhost is working through the heartbeat path. 
 		 */
-		RestAssured.get(HOST+PATH_HEARTBEAT)
-			.then()
-				.statusCode(200)
-				.body("status", Matchers.equalTo("ok"));
+		when()
+			.get(PATH_HEARTBEAT)
+		.then()
+			.statusCode(200)
+			.body("status", Matchers.equalTo("ok"));
 		
 	}
 
@@ -28,9 +38,10 @@ public class SimpleTest {
 		/**
 		 * Here we are checking the status and also the JSON value.
 		 */
-		RestAssured.get(HOST + PATH_SAMPLE)
-			.then()
-				.body("status", Matchers.equalTo("ok"))
-				.body("comments.comment", Matchers.contains("works well"));
+		when()
+			.get(PATH_SAMPLE)
+		.then()
+			.body("status", Matchers.equalTo("ok"))
+			.body("comments.comment", Matchers.contains("works well"));
 	}
 }
